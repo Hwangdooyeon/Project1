28d40b52edfef76d4ecb2fe2058f84dca08197de
Hwangdooyeon <yueuch102@naver.com>
2023-09-20 17:36:15 +0900

    이상한 코드

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 def add_func(n1,n2):
     return n1 + n2
 
+def add_func(n1,n2):
+    return n1 * n2
+
 num1 = 100
 num2 = 200
 
 
 res = add_func(num1, num2)
 print(num1, '+', num2, '=' ,res)
+
+res = add_func(num1, num2)
+print(num1, '*', num2, '=' ,res)
